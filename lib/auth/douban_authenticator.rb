a317781bcda67a3fe862f38a9a4aede0c841f606
Erick Guan <fantasticfears@gmail.com>
2018-11-20 14:29:27 +0100

    Adapt to new third-party login interface

--- lib/auth/douban_authenticator.rb
+++ lib/auth/douban_authenticator.rb
@@ -1,10 +1,15 @@
 class DoubanAuthenticator < ::Auth::Authenticator
-
   def name
     'douban'
   end
 
-  def after_authenticate(auth_token)
+  def enabled?
+    SiteSetting.zh_l10n_enable_douban_logins
+  end
+
+  # run once the user has completed authentication on the third party system. Should return an instance of Auth::Result.
+  # If the user has requested to connect an existing account then `existing_account` will be set
+  def after_authenticate(auth_options, existing_account: nil)
     result = Auth::Result.new
 
     data = auth_token[:info]
@@ -27,9 +32,43 @@ class DoubanAuthenticator < ::Auth::Authenticator
     result
   end
 
+  # can be used to hook in after the authentication process
+  #  to ensure records exist for the provider in the db
+  #  this MUST be implemented for authenticators that do not
+  #  trust email
   def after_create_account(user, auth)
     douban_uid = auth[:extra_data][:douban_uid]
     ::PluginStore.set('douban', "douban_uid_#{douban_uid}", {user_id: user.id})
+  end
+
+  # return a string describing the connected account
+  #  for a given user (typically email address). Used to list
+  #  connected accounts under the user's preferences. Empty string
+  #  indicates not connected
+  def description_for_user(user)
+    ::PluginStore.get('douban', "douban_uid_#{douban_uid}", {user_id: user.id})
+
+    info = FacebookUserInfo.find_by(user_id: user.id)
+    info&.email || info&.username || ""
+  end
+
+  # can authorisation for this provider be revoked?
+  def can_revoke?
+    false
+  end
+
+  # can exising discourse users connect this provider to their accounts
+  def can_connect_existing_user?
+    false
+  end
+
+  # optionally implement the ability for users to revoke
+  #  their link with this authenticator.
+  # should ideally contact the third party to fully revoke
+  #  permissions. If this fails, return :remote_failed.
+  # skip remote if skip_remote == true
+  def revoke(user, skip_remote: false)
+    raise NotImplementedError
   end
 
   def register_middleware(omniauth)
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Discourse 中文本地化服务集合
 # about: 为 Discourse 增加了各种本地化的功能。
-# version: 0.20
+# version: 1.0.0
 # authors: Erick Guan
 # url: https://github.com/fantasticfears/discourse-chinese-localization-pack
 
@@ -9,26 +9,28 @@ enabled_site_setting :zh_l10n_enabled
 # load oauth providers
 Dir[File.expand_path('../lib/auth/*.rb', __FILE__)].each { |f| require f }
 require 'active_support/inflector'
+require "ostruct"
 
 # name, frame_width, frame_height, background_color, glyph
 PROVIDERS = [
-  ['Weibo', 920, 800, 'rgb(230, 22, 45)', '\f18a'],
-  ['QQ', 760, 500, '#51b7ec', '\f1d6'],
-  ['Douban', 380, 460, 'rgb(42, 172, 94)', '豆'],
-  ['Renren', 950, 500, 'rgb(0, 94, 172)', '\f18b']
+  OpenStruct.new(pretty_name: 'Weibo', frame_width: 920, frame_heigh: 800, background_color: 'rgb(230, 22, 45)', glyph: '\f18a'),
+  OpenStruct.new(pretty_name: 'QQ', frame_width: 760, frame_heigh: 500, background_color: '#51b7ec', glyph: '\f1d6'),
+  OpenStruct.new(pretty_name: 'Douban', frame_width: 380, frame_heigh: 460, background_color: 'rgb(42, 172, 94)', glyph: '豆'),
+  OpenStruct.new(pretty_name: 'Renren', frame_width: 950, frame_heigh: 500, background_color: 'rgb(0, 94, 172)', glyph: '\f18b')
 ].freeze
 PLUGIN_PREFIX = 'zh_l10n_'.freeze
 SITE_SETTING_NAME = 'zh_l10n_enabled'.freeze
 ONEBOX_SETTING_NAME = 'zh_l10n_http_onebox_override'.freeze
 
-PROVIDERS.each do |provider|
-  auth_provider authenticator: "#{provider[0]}Authenticator".constantize.new,
-                frame_width: provider[1],
-                frame_height: provider[2],
-                background_color: provider[3],
-                glyph: provider[4],
-                enabled_setting: "#{PLUGIN_PREFIX}enable_#{provider[0].downcase}_logins"
+def _prepare_auth_provide_args(struct)
+  args = struct.to_h
+  name = args[:pretty_name]
+  args[:authenticator] = "#{name}Authenticator".constantize.new
+  args
 end
+
+PROVIDERS.each { |provider| auth_provider _prepare_auth_provide_args(provider) }
+
 Dir[File.expand_path('../lib/onebox_override/*.rb', __FILE__)].each { |f| require f }
 
 after_initialize do
@@ -37,7 +39,7 @@ after_initialize do
   Dir[File.expand_path('../lib/onebox/*.rb', __FILE__)].each { |f| require f }
 
   PROVIDERS.each do |provider|
-    provider_name = provider[0].downcase
+    provider_name = provider.name.downcase
     enable_setting = "#{PLUGIN_PREFIX}enable_#{provider_name}_logins"
     check = "#{provider_name}_config_check".to_sym
 
